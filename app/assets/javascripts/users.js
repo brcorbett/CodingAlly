8fca021822da6d2efc7dab13f52990548aef2a3e
Brock Corbett <brockcorbett@hotmail.com>
2017-01-19 18:58:55 +0000

    Added server side code for creating subscriptions for pro users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,7 @@
 //Document ready.
 $(document).on('turbolinks:load', function(){
     var theForm = $('#pro_form');
-    var submitBtn = $('#form-submit-btn');
+    var submitBtn = $('#form-signup-btn');
     
     //Set Stripe public key.
     Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
@@ -63,7 +63,7 @@ $(document).on('turbolinks:load', function(){
         var token = response.id;
         
         //Inject the card token in a hidden field.
-        theForm.append( $('<input type="hidden" name="user[stripe_card_token]">'),val(token) );
+        theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
         
         //Submit form to our Rails app.
         theForm.get(0).submit();
